3f2ebe0c522af6ec5959d0efb53026aec5e99ca9
chaneyoon <chaneyoon@gmail.com>
2017-07-07 21:07:47 +0900

    add some comment

--- sum_test.c
+++ sum_test.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
  
-int main(void) {
+int main(int argc, char * argv[]) {
+	
+	// i   - for clause
+	// sum - store the sum of 1 ~ 100.
 	int i, sum;
 	
+	// add 1 ~ 100
 	for(i=1; i<=100; i++) {
 		sum += i;
 	}
 	
+	// print the sum of 1 ~ 100.
 	printf("%d\n", sum);
 	
+	// exit
 	return 0;
-	
 }
