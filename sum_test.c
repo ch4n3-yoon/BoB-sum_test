65b0ef8b58e45fbaadd53fa45e572de1f486e30d
chaneyoon <chaneyoon@gmail.com>
2017-07-07 21:17:21 +0900

    add SeockChanYoon's information

--- sum_test.c
+++ sum_test.c
@@ -6,6 +6,11 @@ int main(int argc, char * argv[]) {
 	// sum - store the sum of 1 ~ 100.
 	int i, sum;
 	
+	
+	char track[] = "취약점"; 
+	char name[] = "윤석찬";
+	printf("[bob6][%s]sum_test[%s]\n", track, name);	
+	
 	// add 1 ~ 100
 	for(i=1; i<=100; i++) {
 		sum += i;
